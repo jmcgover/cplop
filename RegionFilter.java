58bfd0556123825efbe53a362a99ac1663935f03
Jeff McGovern <jmcgover@calpoly.edu>
2014-12-01 02:08:58 -0800

    not sure what I changed here

--- RegionFilter.java
+++ RegionFilter.java
@@ -1,8 +1,8 @@
 public class RegionFilter implements Filter<Pyroprint>{
    public RegionFilter(){
-      super();
    }
    public boolean isComparable(Pyroprint a, Pyroprint b){
-      return a.getAppliedRegion().equals(b.getAppliedRegion());
+      return a.isSameAppliedRegion(b);
    }
 }
+
