97365a043e619972bd79cf10b37b8bdcad9da093
Jeff McGovern <jmcgover@calpoly.edu>
2014-12-01 02:05:26 -0800

    compile and run scripts

---
+++ make.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+javac -Xlint:unchecked -cp mysql-connector-java-5.1.34-bin.jar *.java 
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -Xmx2048m -cp mysql-connector-java-5.1.34-bin.jar:. RunTests $@
